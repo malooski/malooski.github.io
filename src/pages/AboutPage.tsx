05d1e6a61389e2e45c7bf2a365f50d5db5397553
Malooski <malooski@protonmail.com>
2022-06-30 02:07:34 -0500

    Update credits page.

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,12 +2,91 @@ import Link from "../components/Link";
 import styled from "styled-components";
 import MyPage from "./MyPage";
 
-const CreditsTable = styled.table`
-    border-collapse: collapse;
+interface CreditEntry {
+    workName: string;
+    workUrl?: string;
 
-    tr {
-        td:first-child {
-            text-align: right;
+    authorName: string;
+    authorUrl?: string;
+}
+
+const CREDITS: CreditEntry[] = [
+    {
+        workName: "Reference",
+        workUrl: "https://twitter.com/malooski_vt/status/1541552859262730241",
+        authorName: "enricaheid",
+        authorUrl: "https://twitter.com/enricaheid",
+    },
+    {
+        workName: "Concept",
+        workUrl: "https://twitter.com/malooski_vt/status/1541805801530064898",
+        authorUrl: "https://twitter.com/ghostaficionado/",
+        authorName: "ghostaficionado",
+    },
+    {
+        workName: "Overlay",
+        workUrl: "https://twitter.com/Delichan3D/status/1538888572769558529",
+        authorUrl: "https://twitter.com/Delichan3D",
+        authorName: "Delichan3D",
+    },
+    {
+        workName: "Twitch Emotes",
+        authorName: "Kavyun",
+        authorUrl: "https://twitter.com/KavyunArt",
+    },
+
+    {
+        workName: "Logo",
+        workUrl: "https://twitter.com/malooski_vt/status/1513497107751133193",
+
+        authorName: "Kota Kotonya",
+        authorUrl: "https://twitter.com/KotaKotonya",
+    },
+    {
+        workName: "Stream PNG",
+        workUrl: "https://twitter.com/malooski_vt/status/1541927930246447104",
+        authorUrl: "https://twitter.com/OneLittleGnome/",
+        authorName: "Ranvi",
+    },
+];
+
+const CREDITS_RADIUS = "1em";
+const CreditsTable = styled.div`
+    display: grid;
+
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    border-radius: ${CREDITS_RADIUS};
+
+    grid-template-columns: auto auto;
+
+    // all cihldren
+    & > div {
+        padding: 1em 1em;
+
+        &:first-child {
+            border-top-left-radius: ${CREDITS_RADIUS};
+        }
+        &:nth-child(2) {
+            border-top-right-radius: ${CREDITS_RADIUS};
+        }
+
+        &:last-child {
+            border-bottom-right-radius: ${CREDITS_RADIUS};
+        }
+        &:nth-last-child(2) {
+            border-bottom-left-radius: ${CREDITS_RADIUS};
+        }
+
+        &:nth-child(2n + 1) {
+        }
+
+        &:nth-child(4n + 1),
+        &:nth-child(4n + 2) {
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+        &:nth-child(4n + 3),
+        &:nth-child(4n) {
+            background-color: rgba(255, 255, 255, 0.05);
         }
     }
 `;
@@ -17,28 +96,26 @@ export default function AboutPage() {
         <MyPage title="About">
             <h3>Credits</h3>
             <CreditsTable>
-                <tbody>
-                    <tr>
-                        <td>3D Model</td>
-                        <td>
-                            <Link href="https://twitter.com/silvercatboi">SilverCatBoi</Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Overlay</td>
-                        <td>
-                            <Link href="https://twitter.com/MyochiMioV">MochiMioV</Link>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Twitch Emotes</td>
-                        <td>
-                            <Link href="https://twitter.com/KavyunArt">Kavyun</Link>
-                        </td>
-                    </tr>
-                </tbody>
+                {CREDITS.map(credit => (
+                    <>
+                        <div>
+                            {credit.workUrl ? (
+                                <Link href={credit.workUrl}>{credit.workName}</Link>
+                            ) : (
+                                <span>{credit.workName}</span>
+                            )}
+                        </div>
+
+                        <div>
+                            {credit.authorUrl ? (
+                                <Link href={credit.authorUrl}>{credit.authorName}</Link>
+                            ) : (
+                                <span>{credit.authorName}</span>
+                            )}
+                        </div>
+                    </>
+                ))}
             </CreditsTable>
-            <hr style={{ width: 200 }} />
             <h3>Site</h3>
             <p>This site uses a collection of different technologies.</p>
             <ul>
