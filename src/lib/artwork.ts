d6850ae184a598851dae9e3278a27fcfeb3e453f
Malooski <malooski@protonmail.com>
2022-08-04 10:13:31 -0500

    Update with new comm.

--- src/lib/artwork.ts
+++ src/lib/artwork.ts
@@ -6,6 +6,7 @@ import soieDanceFanartUrl from "../assets/thumbnails/soie dance fanart.gif";
 import trashpitsComm1Url from "../assets/thumbnails/trashpits comm 1.jpg";
 import trashpitsComm2Url from "../assets/thumbnails/trashpits comm 2.jpg";
 import hatchetCommUrl from "../assets/thumbnails/hatchet comm.jpg";
+import yantiCommUrl from "../assets/thumbnails/yanti comm.jpg";
 
 export enum ArtworkType {
     COMMISSION,
@@ -18,12 +19,23 @@ export interface ArtworkInfo {
     thumbUrls: string[];
     imgUrl: string[];
 
+    title?: string;
+
     workUrl: string;
     authorName: string;
     authorUrl?: string;
 }
 
 export const ARTWORKS: ArtworkInfo[] = [
+    {
+        title: "Wanna chill out?",
+        type: ArtworkType.COMMISSION,
+        thumbUrls: [yantiCommUrl],
+        imgUrl: ["https://malooski-public.s3.us-east-2.amazonaws.com/artwork/yantl+comm.jpg"],
+        workUrl: "https://twitter.com/malooski_vt/status/1555012934916624385",
+        authorName: "Yantl Ayen",
+        authorUrl: "https://www.fiverr.com/yanti_ayen_19",
+    },
     {
         imgUrl: ["https://malooski-public.s3.us-east-2.amazonaws.com/artwork/hatchet+fullbody.jpg"],
         type: ArtworkType.COMMISSION,
--- src/util/hooks/use_throttled_state.ts
+++
@@ -1,9 +0,0 @@
-import { throttle } from "lodash";
-import { useCallback, useState } from "react";
-
-export default function useThrottledState<T>(initialValue: T, ms: number) {
-    const [state, setState] = useState<T>(initialValue);
-    const throttledSetState = useCallback(() => throttle(setState, ms), [ms]);
-
-    return [state, throttledSetState] as const;
-}
