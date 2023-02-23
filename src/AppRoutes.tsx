1fbe44060322ee0ed16f34cd459f21a0c465e466
Malooski <malooski@protonmail.com>
2023-02-22 20:33:57 -0600

    Remove ref pages.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,11 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
-import FullWidthLayout from "./layouts/FullWidth";
 import MainLayout from "./layouts/Main";
 
 import AboutPage from "./pages/AboutPage";
 import ArtworkPage from "./pages/ArtworkPage";
 import ExpandedArtworkPage from "./pages/ExpandedArtworkPage";
 import LinksPage from "./pages/LinksPage/index";
-import RefPage from "./pages/RefPage";
 
 export default function AppRoutes() {
     return (
@@ -19,9 +17,6 @@ export default function AppRoutes() {
                     <Route path="artwork" element={<ArtworkPage />} />
                 </Route>
 
-                <Route path="/" element={<FullWidthLayout />}>
-                    <Route path="ref" element={<RefPage />} />
-                </Route>
                 <Route path="/artwork/:artworkId" element={<ExpandedArtworkPage />} />
                 <Route path="/artwork/:artworkId/:page" element={<ExpandedArtworkPage />} />
             </Routes>
