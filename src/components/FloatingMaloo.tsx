d152129e77e93f5c0731e17667b992671c877e49
Malooski <malooski@protonmail.com>
2023-10-29 14:19:52 -0500

    Fix floating maloo loading.

--- src/components/FloatingMaloo.tsx
+++ src/components/FloatingMaloo.tsx
@@ -3,5 +3,11 @@ import { MALOO_PEEKING_WEBM_URL } from "../constants";
 import * as classes from "./FloatingMaloo.module.scss";
 
 export default function FloatingMaloo() {
+    if (!isDesktop()) return null;
+
     return <video className={classes.maloo} autoPlay muted loop src={MALOO_PEEKING_WEBM_URL} />;
+}
+
+function isDesktop() {
+    return window.innerWidth > 768;
 }
